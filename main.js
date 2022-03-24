2651b6ca834b5d632f0d77a1a27549db4884b1f0
Rhrudayesh <83079097+Rhrudayesh@users.noreply.github.com>
2022-03-24 18:15:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+function setup() {
+Canvas=createCanvas(280,280);
+Canvas.center();
+background("white");
+
+}
+function draw() {
+
+}
+function clear(){
+    background("white"); 
+}
